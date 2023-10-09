410b6bb77277822c5c5fba2ef21b8a8f66648bd3
Choongin Lee <choonginlee@gmail.com>
2023-10-09 12:23:27 +0000

    token renewal: need to modify to run

--- fuzzers/recfuzz_aflplusplus/fetch.sh
+++ fuzzers/recfuzz_aflplusplus/fetch.sh
@@ -5,7 +5,7 @@ set -e
 # Pre-requirements:
 # - env FUZZER: path to fuzzer work dir
 ##
-git clone https://choonginlee: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@github.com/choonginlee/recfuzz.git "$FUZZER/repo" 2>&1
+git clone https://choonginlee:GITHUB_TOKEN@github.com/choonginlee/recfuzz.git "$FUZZER/repo" 2>&1
 git clone --no-checkout https://github.com/google/AFL.git "$FUZZER/repo/afl" 2>&1
 git -C "$FUZZER/repo/afl" checkout 61037103ae3722c8060ff7082994836a794f978e 2>&1
 
